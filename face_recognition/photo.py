5a6be0fe85b1db99df548565cd9d8bc90211f544
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-12-12 12:26:06 +0530

    Create photo.py

---
+++ face_recognition/photo.py
@@ -0,0 +1,23 @@
+import cv2,numpy,os,sys
+haar_file='haarcascade_face.xml'
+##dst='E:\datasets'
+##sub='dhruvi'
+##path=os.path.join(dst,sub)
+##if os.path.isdir(path):
+##    os.mkdir(path)
+path='E:\\dhruvitraining\\face_recognition\\'
+
+(width,height)=(130,20)
+face=cv2.CascadeClassifier(haar_file)
+img=cv2.imread('dhruvi.jpg')
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces=face.detectMultiScale(gray,1.3,3)
+for (x,y,w,h)in faces:
+    cv2.rectangle(img,(x,y),(x+w,h+y),(0,0,255),2)
+    fac=gray[y:y+h,x:x+w]
+    final_face=cv2.resize(fac,(width,height))
+    cv2.imwrite('%s / abc .png' %(path),final_face)
+
+cv2.imshow('OpenCV',img)
+    
+    
